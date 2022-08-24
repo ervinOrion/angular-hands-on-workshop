d7332e30156d14c1f8d038584d0f44191ab0f72d
Ervin Suhanko <ervin.suhanko@orioninc.com>
2022-08-24 15:41:33 +0200

    books page created

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,33 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './models/book.model';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  // template: `<h3>Inline - It is working</h3>`,
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent implements OnInit {
 
-  person = {
-    name: 'Ervin',
-    surname: 'Suhanko'
-  }
-  name = "Angular Workshop";
-  imageUrl = 'https://www.w3schools.com/images/picture.jpg';
+  data: Book[] = [
+      {
+        id: 1,
+        title: "Angular Projects",
+        author: "Aristeidis Bampakos",
+        percentComplete: 26,
+        purchaseLink: "https://www.amzn.com/1800205260",
+        coverUrl: "https://images-na.ssl-images-amazon.com/images/I/41ozUFXCXOS._SX404_BO1,204,203,200_.jpg",
+        favorite: true
+      },
+      {
+        id: 2,
+        title: "Angular Cookbook",
+        author: "Muhammad Ahsan Ayaz",
+        percentComplete: 26,
+        purchaseLink: "https://www.amzn.com/1838989439",
+        coverUrl: "https://images-na.ssl-images-amazon.com/images/I/411QTHyJ12L._SX404_BO1,204,203,200_.jpg",
+        favorite: true
+      },
+      {
+        id: 3,
+        title: "Learning Angular",
+        author: "Aristeidis Bampakos",
+        purchaseLink: "https://www.amazon.com/dp/1839210664",
+        coverUrl: "https://images-na.ssl-images-amazon.com/images/I/41tbeEEyfUL._SX404_BO1,204,203,200_.jpg",
+        favorite: false,
+        percentComplete: 26
+      },
+      {
+        id: 4,
+        title: "JavaScript: The Definitive Guide",
+        author: "David Flanagan",
+        percentComplete: 26,
+        purchaseLink: "https://www.amazon.com/gp/product/1491952024",
+        coverUrl: "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX375_BO1,204,203,200_.jpg",
+        favorite: false
+      },
+      {
+        id: 5,
+        title: "Eloquent JavaScript, 3rd Edition",
+        author: "Marijn Haverbeke",
+        percentComplete: 26,
+        purchaseLink: "https://www.amazon.com/gp/product/1593279507",
+        coverUrl: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+        favorite: true
+      }
+    ]
 
-  clicked: boolean;
-  fontSizePx = 0;
+  selectedBook: Book = null;
 
-  arr =[1, 2, 3, 4];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClick() {
-    this.clicked = !this.clicked;
-    this.fontSizePx = 101;
+  showDetails(book: Book) {
+    this.selectedBook = book;
   }
 
 }
---
+++ src/app/book/models/book.model.ts
@@ -0,0 +1,9 @@
+export interface Book {
+  id?: number;
+  title?: string;
+  author?: string;
+  percentComplete?: number;
+  purchaseLink?: string;
+  coverUrl?: string;
+  favorite?: boolean;
+}
